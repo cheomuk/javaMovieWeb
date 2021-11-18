a0513085f4c95b866b1d265db0b56a03bdbb4fc3
cheomuk <pkw9815@naver.com>
2021-11-19 05:21:13 +0900

    Kakao_API_Setting

--- src/main/java/com/example/controller/signController.java
+++ src/main/java/com/example/controller/signController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import com.example.mapper.kakaoApi;
 
 @RestController
 public class signController {
---
+++ src/main/java/com/example/mapper/kakaoApi.java
@@ -0,0 +1,33 @@
+package com.example.mapper;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class kakaoApi {
+	
+	public String getAccessToken(String code) {
+		String accessToken = "";
+		String refreshToken = "";
+		String reqURL = "https://kauth.kakao.com/oauth/token";
+		
+		try {
+			URL url = new URL(reqURL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return accessToken;
+	}
+}
