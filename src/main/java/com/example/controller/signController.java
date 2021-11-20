47274948b6f5da35e94942ed885e47eb1e6c4bbb
cheomuk <pkw9815@naver.com>
2021-11-21 01:35:37 +0900

    Kakao_LogIn_Prototype

--- src/main/java/com/example/controller/signController.java
+++ src/main/java/com/example/controller/signController.java
@@ -13,19 +13,35 @@ import com.example.mapper.kakaoApi;
 
 @RestController
 public class signController {
-	kakaoApi kakaoapi = new kakaoApi();
+kakaoApi kakaoApi = new kakaoApi();
 	
 	@RequestMapping(value="/login")
 	public ModelAndView login(@RequestParam("code") String code, HttpSession session) {
 		ModelAndView mav = new ModelAndView();
-		String access_token = kakaoapi.getAccessToken(code);
-		HashMap<String, Object> userInfo = kakaoapi.getUserInfo(access_token);
+		// 1번 인증코드 요청 전달
+		String accessToken = kakaoApi.getAccessToken(code);
+		// 2번 인증코드로 토큰 전달
+		HashMap<String, Object> userInfo = kakaoApi.getUserInfo(accessToken);
 		
 		System.out.println("login info : " + userInfo.toString());
+		
 		if(userInfo.get("email") != null) {
 			session.setAttribute("userId", userInfo.get("email"));
-			session.setAttribute("access_token", access_token);
+			session.setAttribute("accessToken", accessToken);
 		}
+		mav.addObject("userId", userInfo.get("email"));
+		mav.setViewName("index");
+		return mav;
+	}
+	
+	@RequestMapping(value="/logout")
+	public ModelAndView logout(HttpSession session) {
+		ModelAndView mav = new ModelAndView();
+		
+		kakaoApi.kakaoLogout((String)session.getAttribute("accessToken"));
+		session.removeAttribute("accessToken");
+		session.removeAttribute("userId");
+		mav.setViewName("index");
 		return mav;
 	}
 }
